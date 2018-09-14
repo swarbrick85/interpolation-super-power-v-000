206d75910ed86f839eafe79b8f84f19853a72c5c
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-09-14 08:40:52 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,3 @@
-# Write your #display_rainbow method here
+def display_rainbow(palate) 
+puts "R: #{palate[0]}, O: #{palate[1]}, Y: #{palate[2]}, G: #{palate[3]}, B: #{palate[4]}, I: #{palate[5]}, V: #{palate[6]}"
+end
