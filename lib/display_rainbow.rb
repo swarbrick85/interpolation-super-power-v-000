7aa88304accf96c8eecef65a2e931d1ebb4af81e
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-09-14 08:38:16 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,9 @@
-# Write your #display_rainbow method here
+def #display_rainbow(palate) 
+puts "R: #{palate[0]}"
+puts "O: #{palate[1]}"
+puts "Y: #{palate[2]}"
+puts "G: #{palate[3]}"
+puts "B: #{palate[4]}"
+puts "I: #{palate[5]}"
+puts "V: #{palate[6]}"
+end
