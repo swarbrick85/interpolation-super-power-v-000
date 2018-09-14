6a817576ade25005e930d3e73cb26b938bbef8e8
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-09-14 08:38:28 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,9 +1,3 @@
 def #display_rainbow(palate) 
-puts "R: #{palate[0]}"
-puts "O: #{palate[1]}"
-puts "Y: #{palate[2]}"
-puts "G: #{palate[3]}"
-puts "B: #{palate[4]}"
-puts "I: #{palate[5]}"
-puts "V: #{palate[6]}"
+puts "R: #{palate[0]}, O: #{palate[1]}, Y: #{palate[2]}, G: #{palate[3]}, B: #{palate[4]}, I: #{palate[5]}, V: #{palate[6]}"
 end
